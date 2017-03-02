8f0fd4c0d042412ecc338a798d8068cf0092a57a
uires deivide <uiresdeivide1@gmail.com>
2017-03-02 13:01:19 -0300

    Aula 3- Extraindo métodos com refatoração

--- src/br/com/caelum/empresa/modelo/ImportadorDeGastos.java
+++ src/br/com/caelum/empresa/modelo/ImportadorDeGastos.java
@@ -12,14 +12,14 @@ import br.com.caelum.curso.Funcionario;
 
 public class ImportadorDeGastos {
 
+	private SimpleDateFormat df = new SimpleDateFormat("ddMMyyyy");
+
 	public List<Gasto> importa(InputStream entrada) throws ParseException {
-		SimpleDateFormat df = new SimpleDateFormat("ddMMyyyy");
-		
+
 		Scanner leitor = new Scanner(entrada);
 		List<Gasto> gastos = new ArrayList<Gasto>();
-		
+
 		while (leitor.hasNextLine()) {
-			
 			String linhas = leitor.nextLine();
 			String tipoDeGastos = linhas.substring(0, 6);
 			String dataGastoTxt = linhas.substring(6, 14);
@@ -27,22 +27,28 @@ public class ImportadorDeGastos {
 			String matriculaTxt = linhas.substring(23, 26);
 			String nome = linhas.substring(26, 56);
 			String dataNascTxt = linhas.substring(56);
-			
+
 			double valor = Double.parseDouble(valortxt);
-			
+
 			int matricula = Integer.parseInt(matriculaTxt);
-						
-			Calendar dataNascimento = Calendar.getInstance();			
-			dataNascimento.setTime(df.parse(dataNascTxt));
-			
-			Calendar dataDespesa = Calendar.getInstance();
-			dataDespesa.setTime(df.parse(dataGastoTxt));
-			
+
+			Calendar dataNascimento = converteDataTxtParaCalendar(dataNascTxt);
+
+			Calendar dataDespesa = converteDataTxtParaCalendar(dataGastoTxt);
+
 			Funcionario funcionario = new Funcionario(nome, matricula,
 					dataNascimento);
 			gastos.add(new Gasto(valor, tipoDeGastos, funcionario, dataDespesa));
 		}
 		leitor.close();
 		return gastos;
+
+	}
+
+	private Calendar converteDataTxtParaCalendar(String dataNascTxt)
+			throws ParseException {
+		Calendar dataNascimento = Calendar.getInstance();
+		dataNascimento.setTime(df.parse(dataNascTxt));
+		return dataNascimento;
 	}
 }
